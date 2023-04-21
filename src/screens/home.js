5ff4f23ebed4b868a8a2a49a4c332ab9d1ca36fd
Ashish Trehan <ashishtrehan002@gmail.com>
2023-04-21 23:59:41 +0530

    added media queries for larger screens

--- src/screens/home.js
+++ src/screens/home.js
@@ -14,18 +14,17 @@ export default function Home() {
     <>
       <Navbar />
       <section id="intro">
-        <div style={{ color: 'gray', fontSize: "1.1em" }}>
+        <div style={{ color: 'gray', fontSize: "1.1em" }} className='introText'>
           HELLO!
         </div>
-        <div style={{ fontSize: "1.7em" }}>
-          I'm <span style={{ fontWeight: "bolder" }}>Ashish
+        <div style={{ fontSize: "1.7em" }}  className='introText'>
+          I'm <span style={{ fontWeight: "bolder" }}  className='introText'>Ashish
             <br />
           </span>
-          <strong style={{ fontWeight: "bolder" }}>a</strong> Full Stack Developer
+          <strong style={{ fontWeight: "bolder" }}  className='introText'>a</strong> Full Stack Developer
         </div>
-        <div style={{ width: "100%", fontSize: "1em" }}>
+        <div style={{  fontSize: "1em" }}  className='introText'>
           Lorem ipsum dolor sit amet consectetur adipisicing
-          <br />
           elit. Aperiam, eiu consectetur adipisicing elit. Fugit, quis.
         </div>
         <div style={{ textAlign: 'center' }}>
@@ -33,6 +32,25 @@ export default function Home() {
           <img src={image} alt="" className='introImg' />
         </div>
       </section>
+
+{/* Intro for larger screen sizes */}
+      <div className='maindiv'>
+        <p className=' container'><img src={image} alt="Not Found" style={{ float: "right", width: "250px", height: "250px", marginLeft: "15px", marginRight: "50px" }} />
+          <div style={{ color: 'gray', fontSize: "1.1em" }}>
+            HELLO!
+          </div>
+          <div style={{ fontSize: "1.7em" }} >
+            I'm <span style={{ fontWeight: "bolder" }}>Ashish
+              <br />
+            </span>
+            <strong  style={{ fontWeight: "bolder" }}>a</strong> Full Stack Developer
+          </div>
+          <div style={{  fontSize: "1em" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, eius. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, quis.
+          </div>
+        </p>
+      </div>
+
       <TechStackScreen/>
       <CodingProfilesScreen/>
       <About />
--- src/screens/projects.js
+++ src/screens/projects.js
@@ -5,37 +5,38 @@ import gamezone from '../images/gamezone.png'
 import gofood from '../images/food.png'
 
 export default function Projects() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            title: "Food Delivery App",
-            desc: "It is a responsive food delivery application based on MERN stack. Here, a user can register/login and the make use of the dynamic data to add food items to their cart. Various authentications are provided using tools such as JWT etc.",
-            image: gofood,
-            github: "https://github.com/ashish-2409/Food-Delivery-App",
-            live: "",
-            techUsed:[
-                'Reactjs',
-                'MongoDB',
-                'Nodejs',
-            ]
-        },
-        {
-            id: 2,
-            title: "GameZone App",
-            desc: "It is a review logging, cross platform application created using react-native. Here a user can add new reviews and then delete them easily. Other modules such as Formik and yup are used for forms and validations. Being cross platform, the same code can be used to run on android, ios and web platforms.", 
-            image: gamezone,
-            github: "https://github.com/ashish-2409/GameZone-app",
-            live: "",
-            techUsed:[
-                'react-native',
-                'android'
-            ]
-        },
+  const [data, setData] = useState([
+    {
+      id: 1,
+      title: "Food Delivery App",
+      desc: "It is a responsive food delivery application based on MERN stack. Here, a user can register/login and the make use of the dynamic data to add food items to their cart. Various authentications are provided using tools such as JWT etc.",
+      image: gofood,
+      github: "https://github.com/ashish-2409/Food-Delivery-App",
+      live: "",
+      techUsed: [
+        'Reactjs',
+        'MongoDB',
+        'Nodejs',
+      ]
+    },
+    {
+      id: 2,
+      title: "GameZone App",
+      desc: "It is a review logging, cross platform application created using react-native. Here a user can add new reviews and then delete them easily. Other modules such as Formik and yup are used for forms and validations. Being cross platform, the same code can be used to run on android, ios and web platforms.",
+      image: gamezone,
+      github: "https://github.com/ashish-2409/GameZone-app",
+      live: "",
+      techUsed: [
+        'react-native',
+        'android'
+      ]
+    },
 
-    ]);
+  ]);
 
-    return (
-        <section id="projects">
+  return (
+    <>
+      <section id="projects">
         <div style={{ fontSize: "1em", width: "80%", textAlign: 'center', color: "blue", fontFamily: "ExtraBold", fontWeight: "bolder", marginTop: "150px", margin: 'auto' }}>
           PROJECTS
           <div style={{ fontSize: "1em", color: "black", fontFamily: "ExtraBold", fontWeight: "bolder" }}>
@@ -85,5 +86,52 @@ export default function Projects() {
           })
         }
       </section>
-    )
+
+        {/* For larger screens */}
+
+      <div id="projects2">
+        <div className='container' style={{ fontSize: "13.5px", width: "700px", alignContent: "center", color: "blue", fontFamily: "ExtraBold", fontWeight: "bolder", marginTop: "90px" }}>
+          PROJECTS
+          <div style={{ fontSize: "18px", color: "black", fontFamily: "ExtraBold", fontWeight: "bolder" }}>
+            Each project is a unique piece of development
+          </div>
+        </div>
+        {
+          data.map((e) => {
+            return (
+              <div className='projectdiv2'>
+                <p className=' container'><img src={e.image} alt="Not Found" className={
+                  ((e.id) % 2 ? "imageLeft2" : "imageRight2")
+                } />
+                  <div style={{ fontSize: "15px", fontFamily: "ExtraBold", fontWeight: "bolder" }}>
+                    <span style={{ fontWeight: "bold" }}>{e.title}
+                    </span>
+                  </div>
+                  <div style={{ fontSize: "13px", marginTop: "20px", fontFamily: "Regular" }}>
+                    {e.desc}
+                  </div>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    {
+                      e.techUsed.map((tech) => {
+                        return (
+                          <span style={{ fontSize: "14px", fontFamily: "ExtraBold", fontWeight: "bolder", padding: "10px" }}>
+                            {
+                              tech
+                            }
+                          </span>
+                        )
+                      })
+                    }
+                  </div>
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <a href={e.github} target='__blank' style={{ textDecoration: "none", color: "black" }}> <span style={{ fontSize: "12px", fontFamily: "Bold", fontWeight: "bold" }}>Code Here</span><img src={github} alt="" style={{ height: "30px" }} /></a>
+                  </div>
+                </p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
 }
