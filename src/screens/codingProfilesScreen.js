ec517673f331e6a6ab2c76cb0627eb16e44f5dbf
Ashish Trehan <ashishtrehan002@gmail.com>
2023-04-22 00:39:07 +0530

    changes to media queries

--- src/screens/codingProfilesScreen.js
+++ src/screens/codingProfilesScreen.js
@@ -17,6 +17,19 @@ export default function codingProfilesScreen() {
           }
         </div>
       </section>
+          {/* for larger screens */}
+      <section id="codingProfiles2">
+        <h4>
+          Coding Profiles
+        </h4>
+        <div className="grid5col2">
+    <a href={codingProfiles[0].link}  style={{gridColumn:'3'}}><img src={codingProfiles[0].img} className='profileImg' alt={codingProfiles[0].title} /></a>
+    <a href={codingProfiles[1].link}><img src={codingProfiles[1].img} className='profileImg' alt={codingProfiles[1].title} /></a>
+    <a href={codingProfiles[2].link}><img src={codingProfiles[2].img} className='profileImg' alt={codingProfiles[2].title} /></a>
+    <a href={codingProfiles[3].link}><img src={codingProfiles[3].img} className='profileImg' alt={codingProfiles[3].title} /></a>
+    <a href={codingProfiles[4].link}><img src={codingProfiles[4].img} className='profileImg' alt={codingProfiles[4].title} /></a>
+        </div>
+      </section>
     </div>
   )
 }
--- src/screens/techStackScreen.js
+++ src/screens/techStackScreen.js
@@ -17,6 +17,20 @@ export default function TechStackScreen() {
                     }
                 </div>
             </section>
+            {/* for larger screens */}
+            <section id="techStack2">
+                <h4>
+                    Tech Stack
+                </h4>
+                <div className="grid2">
+                    <img src={techStack[0].img} alt={techStack[0].title} className='techImg2' style={{gridColumn:'3'}}/>
+                    <img src={techStack[1].img} alt={techStack[1].title} className='techImg2' />
+                    <img src={techStack[2].img} alt={techStack[2].title} className='techImg2' />
+                    <img src={techStack[3].img} alt={techStack[3].title} className='techImg2' />
+                    <img src={techStack[4].img} alt={techStack[4].title} className='techImg2' />
+                    <img src={techStack[5].img} alt={techStack[5].title} className='techImg2' />
+                </div>
+            </section>
         </div>
     )
 }
