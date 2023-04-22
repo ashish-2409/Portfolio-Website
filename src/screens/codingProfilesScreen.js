1eb2a16e66d4197d94540c9358160a0d70f1af1d
Ashish Trehan <ashishtrehan002@gmail.com>
2023-04-22 12:40:30 +0530

    improved tech and profile sections

--- src/screens/codingProfilesScreen.js
+++ src/screens/codingProfilesScreen.js
@@ -23,12 +23,16 @@ export default function codingProfilesScreen() {
           Coding Profiles
         </h4>
         <div className="grid5col2">
-    <a href={codingProfiles[0].link}  style={{gridColumn:'3'}}><img src={codingProfiles[0].img} className='profileImg' alt={codingProfiles[0].title} /></a>
-    <a href={codingProfiles[1].link}><img src={codingProfiles[1].img} className='profileImg' alt={codingProfiles[1].title} /></a>
-    <a href={codingProfiles[2].link}><img src={codingProfiles[2].img} className='profileImg' alt={codingProfiles[2].title} /></a>
-    <a href={codingProfiles[3].link}><img src={codingProfiles[3].img} className='profileImg' alt={codingProfiles[3].title} /></a>
-    <a href={codingProfiles[4].link}><img src={codingProfiles[4].img} className='profileImg' alt={codingProfiles[4].title} /></a>
-        </div>
+          {/* Dummy div tag */}
+          <div style={{gridColumn:'2'}}></div>
+          {
+            codingProfiles.map((e) => {
+              return (
+                <a href={e.link}><img src={e.img} className='profileImg' alt={e.title} /></a>
+              )
+            })
+          }
+        </div> 
       </section>
     </div>
   )
--- src/screens/techStackScreen.js
+++ src/screens/techStackScreen.js
@@ -23,12 +23,14 @@ export default function TechStackScreen() {
                     Tech Stack
                 </h4>
                 <div className="grid2">
-                    <img src={techStack[0].img} alt={techStack[0].title} className='techImg2' style={{gridColumn:'3'}}/>
-                    <img src={techStack[1].img} alt={techStack[1].title} className='techImg2' />
-                    <img src={techStack[2].img} alt={techStack[2].title} className='techImg2' />
-                    <img src={techStack[3].img} alt={techStack[3].title} className='techImg2' />
-                    <img src={techStack[4].img} alt={techStack[4].title} className='techImg2' />
-                    <img src={techStack[5].img} alt={techStack[5].title} className='techImg2' />
+                    <div style={{gridColumn:'2'}}></div> {/* dummy tag */}
+                    {
+                        techStack.map((e) => {
+                            return (
+                                <img src={e.img} alt={e.title} className='techImg' />
+                            )
+                        })
+                    }
                 </div>
             </section>
         </div>
